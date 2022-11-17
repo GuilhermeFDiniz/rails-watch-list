b8140b17d53eeaf00277354815fa4c3e5b6c833f
Guilherme Diniz <guidinizeng@gmail.com>
2022-11-17 16:06:11 -0300

    linked bookmark to list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'net/http'
+require 'json'
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+uri = URI(url)
+response = Net::HTTP.get(uri)
+json = JSON.parse(response)
+
+puts "Cleaning Database"
+Movie.destroy_all
+puts "Creating a new Database"
+
+json["results"].each do |movie|
+  new_movie = Movie.new(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/original/#{movie["poster_path"]}", rating: movie["vote_average"])
+puts "Movie: #{new_movie.title} created "
+if new_movie.save
+  puts "Saved"
+else
+  puts "something went wrong"
+end
+end
+
+puts "Finished"
